8e40fc5987c9fd91ab322cf276904fd58b40d356
Paul Austin <paul.austin@revolsys.com>
2017-05-26 08:50:53 -0700

    completed migration

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,7 +10,12 @@ import {
   Response
 } from '@angular/http';
 
-import { Map } from 'leaflet';
+import {
+  CircleMarker,
+  GeoJSON,
+  Map,
+  Popup
+} from 'leaflet';
 import * as L from 'leaflet';
 import { TiledMapLayer } from 'esri-leaflet';
 
@@ -26,70 +31,24 @@ export class MapComponent implements OnInit {
   
   @ViewChild('map') mapElement: ElementRef;
 
-  /*-----BASE MAPS-----*/
-  private provRoadsWM = new TiledMapLayer({
-    url: this.bcArcGisRestUrl + '/province/roads_wm/MapServer',
-    useCors: false
-  });
-  
-  
-  private provWebMercatorCache = new TiledMapLayer({
-    url: this.bcArcGisRestUrl + '/province/web_mercator_cache/MapServer',
-    useCors: false
-  });
-
-  /*-----POINTS OF DIVERSION-----*/  
-  private pointsOfDiversion = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_WATER_MANAGEMENT.WLS_POD_LICENCE_SP/ows', {
-    layers: 'pub:WHSE_WATER_MANAGEMENT.WLS_POD_LICENCE_SP',
-      format: 'image/png',
-      transparent: true,
-      attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
-    styles: 'Points_of_Diversion'
-  });
-  
-  /*-----FWA WATERSHED GROUPS POLY-----*/  
-  private fwaWatershedGroups = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY/ows', {
-    layers: 'pub:WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY',
-      format: 'image/png',
-      transparent: true,
-      attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
-    styles: 'FWA_Watershed_Groups_Outlined'
-  });
-  
-  /*-----FWA WATERSHED GROUPS LABELS-----*/  
-  private fwaWatershedGroupsLabels = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY/ows', {
-    layers: 'pub:WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY',
-      format: 'image/png',
-      transparent: true,
-      attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
-    styles: 'FWA_Watershed_Groups_Labels'
-  });
-  
-  /*-----FWA WATERSHED GROUPS: LAYER GROUP-----*/
-  private fwaWatershedGroups_LyrGrp = new L.layerGroup([this.fwaWatershedGroups, this.fwaWatershedGroupsLabels]);
-      
-  /*-----FWA ASSESSMENT WATERSHEDS-----*/  
-  private fwaAssessmentWatersheds = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_ASSESSMENT_WATERSHEDS_POLY/ows', {
-    layers: 'pub:WHSE_BASEMAPPING.FWA_ASSESSMENT_WATERSHEDS_POLY',
-      format: 'image/png',
-      transparent: true,
-      attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
-    styles: 'FWA_Assessment_Watersheds_Outlined'
-  });
-  
-  /*-----WATER RESOURCE MANAGMENT POINTS-----*/  
-  wtrResourceMgmtPoints = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_WATER_MANAGEMENT.WLS_WATER_RESOURCE_MGMT_POINT/ows', {
-      layers: 'pub:WHSE_WATER_MANAGEMENT.WLS_WATER_RESOURCE_MGMT_POINT',
-        format: 'image/png',
-        transparent: true,
-        attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
-      styles: 'Protected_Rivers_Points'
-  });
+  layerControl : L.control;
   
   highlightedRiver : any;
   
-  rivers :any;
+  riverLayer :GeoJSON;
   
+  riverPopup : Popup;
+  
+  riverSource = 0;
+  
+  highlightedEmsStation : any;
+  
+  emsStationLayer :GeoJSON;
+  
+  emsStationPopup : Popup;
+
+  emsStationSource = 0;
+
   map : Map;
   
   constructor(
@@ -106,6 +65,11 @@ export class MapComponent implements OnInit {
         [60.1, -113.9]
       ],
     });
+    
+    L.control.scale({
+      imperial: false
+    }).addTo(map);
+    
     let boundsHandler = ()=> {
       map.setMinZoom( map.getBoundsZoom( map.options.maxBounds ) );
     };
@@ -113,26 +77,92 @@ export class MapComponent implements OnInit {
     map.on('resize', boundsHandler);
     map.fitWorld();
 
-    this.provRoadsWM.addTo(map);
-    this.provWebMercatorCache.addTo(map);
-    this.pointsOfDiversion.addTo(map);
-    this.fwaWatershedGroups.addTo(map);
-    this.fwaWatershedGroupsLabels.addTo(map);
-    this.fwaWatershedGroups_LyrGrp.addTo(map);
-    this.wtrResourceMgmtPoints.addTo(map);
-    
+    this.baseLayersInit();
     this.riverInit();
+    this.emsStationInit();
+    this.tempLayersInit();
+  }
+
+  private baseLayersInit() {
+    const map = this.map;
+
+    /*-----BASE MAPS-----*/
+    const provRoadsWM = new TiledMapLayer({
+      url: this.bcArcGisRestUrl + '/province/roads_wm/MapServer',
+      useCors: false
+    });
     
+    
+    const provWebMercatorCache = new TiledMapLayer({
+      url: this.bcArcGisRestUrl + '/province/web_mercator_cache/MapServer',
+      useCors: false
+    });
+
+    /*-----POINTS OF DIVERSION-----*/  
+    const pointsOfDiversion = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_WATER_MANAGEMENT.WLS_POD_LICENCE_SP/ows', {
+      layers: 'pub:WHSE_WATER_MANAGEMENT.WLS_POD_LICENCE_SP',
+        format: 'image/png',
+        transparent: true,
+        attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
+      styles: 'Points_of_Diversion'
+    });
+    
+    /*-----FWA WATERSHED GROUPS POLY-----*/  
+    const fwaWatershedGroups = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY/ows', {
+      layers: 'pub:WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY',
+        format: 'image/png',
+        transparent: true,
+        attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
+      styles: 'FWA_Watershed_Groups_Outlined'
+    });
+    
+    /*-----FWA WATERSHED GROUPS LABELS-----*/  
+    const fwaWatershedGroupsLabels = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY/ows', {
+      layers: 'pub:WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY',
+        format: 'image/png',
+        transparent: true,
+        attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
+      styles: 'FWA_Watershed_Groups_Labels'
+    });
+    
+    /*-----FWA WATERSHED GROUPS: LAYER GROUP-----*/
+    const fwaWatershedGroups_LyrGrp = new L.layerGroup([fwaWatershedGroups, fwaWatershedGroupsLabels]);
+        
+    /*-----FWA ASSESSMENT WATERSHEDS-----*/  
+    const fwaAssessmentWatersheds = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_BASEMAPPING.FWA_ASSESSMENT_WATERSHEDS_POLY/ows', {
+      layers: 'pub:WHSE_BASEMAPPING.FWA_ASSESSMENT_WATERSHEDS_POLY',
+        format: 'image/png',
+        transparent: true,
+        attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
+      styles: 'FWA_Assessment_Watersheds_Outlined'
+    });
+    
+    /*-----WATER RESOURCE MANAGMENT POINTS-----*/  
+    const wtrResourceMgmtPoints = new L.tileLayer.wms(this.bcWmsUrl + '/WHSE_WATER_MANAGEMENT.WLS_WATER_RESOURCE_MGMT_POINT/ows', {
+        layers: 'pub:WHSE_WATER_MANAGEMENT.WLS_WATER_RESOURCE_MGMT_POINT',
+          format: 'image/png',
+          transparent: true,
+          attribution: '© 2013-2016 GeoBC, DataBC, The Province of British Columbia',
+        styles: 'Protected_Rivers_Points'
+    });
+    
+    provRoadsWM.addTo(map);
+    provWebMercatorCache.addTo(map);
+    pointsOfDiversion.addTo(map);
+    fwaWatershedGroups.addTo(map);
+    fwaWatershedGroupsLabels.addTo(map);
+    fwaWatershedGroups_LyrGrp.addTo(map);
+    wtrResourceMgmtPoints.addTo(map);
 
     /*-----Layer Control-----*/
-    var layerControl = L.control.layers({
-        'Roads Base Map': this.provRoadsWM,
-        'Terrain Base Map': this.provWebMercatorCache
+    this.layerControl = L.control.layers({
+        'Roads Base Map': provRoadsWM,
+        'Terrain Base Map': provWebMercatorCache
     },
     {
-      'Points of Diversion (Scale Dependent)': this.pointsOfDiversion,
-      'FWA Watershed Groups (Scale Dependent)': this.fwaWatershedGroups_LyrGrp,
-      'FWA Assessment Watersheds (Scale Dependent)': this.fwaAssessmentWatersheds
+      'Points of Diversion (Scale Dependent)': pointsOfDiversion,
+      'FWA Watershed Groups (Scale Dependent)': fwaWatershedGroups_LyrGrp,
+      'FWA Assessment Watersheds (Scale Dependent)': fwaAssessmentWatersheds
     },
     {
       collapsed: false  
@@ -140,23 +170,37 @@ export class MapComponent implements OnInit {
   }
 
   private riverInit() {
-    const self = this;
-    this.rivers = L.geoJson([], {
+    this.riverLayer = L.geoJson([], {
       style: this.riverStyle.bind(this),
       onEachFeature: (feature, layer) => {
         layer.on({
           mouseover: this.riverMouseOver.bind(this),
-          mouseout: this.riverMouseOut.bind(this)
+          mouseout: this.riverMouseOut.bind(this),
+          click: this.riverClick.bind(this)
         });
       }
-    });
-    this.http.get('assets/FWA_BC_200M.geojson').toPromise().then(response => {
-      const json = response.json();
-      this.rivers.addData(json);
-    });
-    this.rivers.addTo(this.map);
+    })
+     .addTo(this.map)
+    ;
+    this.layerControl.addOverlay(this.riverLayer, 'FWA Stream Network');
+    const loadHandler = (e) => {
+      const zoom = this.map.getZoom();
+      if (zoom >= 10) {
+        if (this.riverSource != 1) {
+          this.loadJson(this.riverLayer, 'assets/QUES_2O_NET10M.geojson');
+          this.riverSource = 1;
+        }
+      } else if (zoom <= 9) {
+        if (this.riverSource != 2) {
+          this.loadJson(this.riverLayer, 'assets/FWA_BC_200M.geojson');
+          this.riverSource = 2;
+        }
+      }
+    };
+    this.map.on('zoomend', loadHandler.bind(this));
+    loadHandler(null);
   }
-
+  
   private riverStyle(river) {
     var color = "#000000";
     var weight = 1;
@@ -190,21 +234,167 @@ export class MapComponent implements OnInit {
 
   private riverMouseOver(e) {
     var layer = e.target;
-    if (this.rivers) {
+    if (this.riverLayer) {
       var river = layer.feature;
       if (river) {
         this.highlightedRiver = river;
       } else {
         this.highlightedRiver = undefined;
       }
-      this.rivers.setStyle(this.riverStyle.bind(this));
+      this.riverLayer.setStyle(this.riverLayer.options.style);
     }
   }
 
   private riverMouseOut(e) {
     this.highlightedRiver = undefined;
-    if (this.rivers) {
-      this.rivers.setStyle(this.riverStyle.bind(this));
+    if (this.riverLayer) {
+      this.riverLayer.setStyle(this.riverLayer.options.style);
     }
+    if (this.riverPopup) {
+      this.map.removeLayer(this.riverPopup);
+      this.riverPopup = null;
+    }
+  }
+  
+  private riverClick(e) {
+    var layer = e.target;
+    var river = layer.feature;
+    if (river) {
+      var latlng = layer.getBounds().getCenter();
+      
+      this.riverPopup = new Popup({
+        offset: [10, -10],
+        closeButton: false
+      })
+        .setLatLng(latlng)
+        .setContent('<b>' + String(river.properties.name) + '</b>' + '<br>Segment length: ' + (river.properties.seglen/1000).toFixed(1) + ' km' + '<br>Upstream length: ' + (river.properties.upslen/1000).toFixed(1) + ' km' + '<br>Downstream length: ' + (river.properties.dwnslen/1000).toFixed(1) + ' km')
+        .addTo(this.map)
+      ;
+    }
+  }
+
+  private emsStationInit() {
+    this.emsStationLayer = L.geoJson([], {
+      pointToLayer: function(feature, latlng) {
+        return new CircleMarker(latlng, {
+          radius: 6,
+          fillColor: '#ff7800',
+          color: '#000',
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8 
+        });
+      },
+      onEachFeature: (feature, layer) => {
+        layer.on({
+          mouseover: this.emsStationMouseOver.bind(this),
+          mouseout: this.emsStationMouseOut.bind(this),
+          click: this.emsStationClick.bind(this)
+        });
+      }
+    })
+      .addTo(this.map)
+    ;
+    this.layerControl.addOverlay(this.emsStationLayer, 'Environmental Monitoring System Station');
+    const loadHandler = (e) => {
+      const zoom = this.map.getZoom();
+      if (zoom >= 10) {
+        if (this.emsStationSource != 1) {
+          this.loadJson(this.emsStationLayer, 'assets/EMS_Monitoring_Locations_QUES.geojson');
+          this.emsStationSource = 1;
+        }
+      } else if (zoom <= 9) {
+        if (this.emsStationSource != 2) {
+          this.emsStationLayer.clearLayers();
+          this.emsStationSource = 2;
+        }
+      }
+    };
+    this.map.on('zoomend', loadHandler.bind(this));
+    loadHandler(null);
+  }
+
+  private emsStationStyle(emsStation) {
+    var color = '#000000';
+    var weight = 1;
+    const highlightedEmsStation = this.highlightedEmsStation;
+    if (highlightedEmsStation) {
+      var emsStation_Id = emsStation.properties.MONITORING_LOCATION_ID;
+      var highlightedEmsStationId = highlightedEmsStation.properties.MONITORING_LOCATION_ID;
+      if (highlightedEmsStationId == emsStation_Id) {
+        color = '#00FFFF';
+        weight = 10;
+      } else {
+        var highlightedEmsStationDescendentIds = highlightedEmsStation.properties.d;
+        if (highlightedEmsStationDescendentIds && highlightedEmsStationDescendentIds.includes(emsStation_Id)) {
+          color = '#FF0000';
+          weight = 5;
+        } else {
+          var highlightedEmsStationAncestorsIds = highlightedEmsStation.properties.a;
+          if (highlightedEmsStationAncestorsIds && highlightedEmsStationAncestorsIds.includes(emsStation_Id)) {
+            color = '#00FF00';
+            weight = 5;
+          }
+        }
+      }
+    }
+    return {
+      color: color,
+      weight: weight
+    };
+  }
+
+  private emsStationMouseOver(e) {
+    var layer = e.target;
+    var emsStation = layer.feature;
+    if (emsStation) {
+      this.highlightedEmsStation = emsStation;
+    } else {
+      this.highlightedEmsStation = undefined;
+    }
+    this.emsStationLayer.setStyle(this.emsStationStyle.bind(this));
+  }
+
+  private emsStationMouseOut(e) {
+    this.highlightedEmsStation = undefined;
+    this.emsStationLayer.setStyle(this.emsStationStyle.bind(this));
+    if (this.emsStationPopup) {
+      this.map.removeLayer(this.emsStationPopup);
+      this.emsStationPopup = null;
+    }
+  }
+
+  private emsStationClick(e) {
+    var layer = e.target;
+    var emsStation = layer.feature;
+    if (emsStation) {
+      var latlng = [emsStation.geometry.coordinates[1], emsStation.geometry.coordinates[0]];
+
+      this.emsStationPopup = new Popup({
+          offset: [10, -10],
+          closeButton: false
+      })
+        .setLatLng(latlng)
+        .setContent('<b>' + String(emsStation.properties.MONITORING_LOCATION_ID) + '</b>' + '<br>Local Watershed Area: ' + (emsStation.properties.WATERSHED_AREA / 100).toFixed(1) + ' sq km')
+        .addTo(this.map)
+      ;
+    }
+  }
+  private tempLayersInit() {
+    /*-----MT. POLLEY MINE MARKER-----*/
+    var mtPolleyMarker = new CircleMarker([52.513437,-121.596309], {
+      title: 'Mt. Polley Mine',
+      weight: 7
+    }).addTo(this.map);
+    mtPolleyMarker.bindPopup('Mt. Polley Mine').openPopup();
+  }
+  
+  private loadJson(layer : GeoJSON, file : string) {
+    this.http.get(file).toPromise().then(response => {
+      console.log(file);
+      layer.clearLayers();
+      const json = response.json();
+      layer.addData(json);
+    });
   }
 }
